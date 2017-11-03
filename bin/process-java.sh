43676bfd8d165c7b5f160184fb61f752b5e64ee0
kongra <kongra@gmail.com>
2017-11-03 23:17:54 +0100

    Script execution flags edited.

--- bin/process-java.sh
+++ bin/process-java.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-java -Xms128m -Xmx4G \
-    -classpath lib/antlr-4.7-complete.jar:lib/aptell.jar \
-    aptell.antlr4.JavaProcessor $1 $2
+java -server -d64 \
+     -Xshare:off -XX:+UseCompressedOops \
+     -XX:+AggressiveOpts -XX:+DoEscapeAnalysis \
+     -Xms128M -Xmx4G -XX:+UseParallelGC -XX:+UseParallelOldGC \
+     -classpath lib/antlr-4.7-complete.jar:lib/aptell.jar \
+     aptell.antlr4.JavaProcessor $1 $2
 
 # Running
 # bin/process-java.sh srcDir targetDir
