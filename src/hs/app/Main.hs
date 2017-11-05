31b79577b438b60144a88e011c2558db4e2f8129
kongra <kongra@gmail.com>
2017-11-05 14:28:23 +0100

    Stack project configuration added.

---
+++ src/hs/app/Main.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE Safe #-}
+------------------------------------------------------------------------
+-- |
+-- Module      : Main
+-- Copyright   : (c) Konrad Grzanek
+-- License     : BSD-style (see the file LICENSE)
+-- Created     : 2017-11-05
+-- Maintainer  : kongra@gmail.com
+-- Stability   : experimental
+-- Portability : portable
+------------------------------------------------------------------------
+module Main (main) where
+
+main :: IO ()
+main = do
+  print "aptell's up and running"
